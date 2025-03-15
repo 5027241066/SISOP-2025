1f5f878024ef82a897f9e5be9f5bcbe959d855e1
5027241066 <5027241066@student.its.ac.id>
2025-03-15 11:36:49 +0700

    poppo_siroyo.sh

--- Sisop-1-2025-IT24/soal_1/poppo_siroyo.sh
+++ Sisop-1-2025-IT24/soal_1/poppo_siroyo.sh
@@ -17,7 +17,7 @@ awk -F, '{
 awk -F, '{if ($8 == "Tablet") total += $6; count++} END {print "Rata-rata durasi membaca dengan Tablet adalah", total/count, "menit."}' reading_data.csv
 
 #c
-
+awk -F, '{if ($7 > max && $7 >= 0 && $7 <= 10) {max = $7; name = $2; book = $3}} END {print "Pembaca dengan rating tertinggi:", name, "-", book, "-", max}' reading_data.csv
 
 #d
 awk -F, '$9 ~ /Asia/ && $5 > "2023-12-31" {genre[$4]++} 

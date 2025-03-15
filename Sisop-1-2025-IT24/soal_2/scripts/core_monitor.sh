04898f909ac61e4b98e415b3b917ae425683f4c8
5027241066 <5027241066@student.its.ac.id>
2025-03-15 12:42:54 +0700

    Commit core_monitor.sh

--- Sisop-1-2025-IT24/soal_2/scripts/core_monitor.sh
+++ Sisop-1-2025-IT24/soal_2/scripts/core_monitor.sh
@@ -1,3 +1,3 @@
-echo "CPU Model: $(lscpu | grep -m1 'Model name' | cut -d ':' -f2 | xargs)"
+echo "CPU Model: $(lscpu | grep -m1 'Model name' | cut -d ':' -f2)"
 echo "CPU Usage: $(top -bn1 | awk '/Cpu\(s\)/ {print 100 - $8}')%"
 echo "Terminal: $(tty)"
